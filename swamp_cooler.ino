816089494ed8c67a7cb267dfb659aa6f0febb122
1100-pierce-louis <Louis_Pierce@nevada.unr.edu>
2025-04-14 12:08:12 -0700

    intial try

--- swamp_cooler.ino
+++ swamp_cooler.ino
@@ -1,7 +1,20 @@
+int buttonPin = 2;
+volatile int count = 0;
+
 void setup() {
-    // Setup Function
+  Serial.begin(9600);
+  pinMode(buttonPin, INPUT_PULLUP);  // Set the button pin as input with pull-up
+  attachInterrupt(digitalPinToInterrupt(buttonPin), countButtonPresses, FALLING);
 }
 
 void loop() {
-    // Looping Function
+  Serial.print("Button Press Count: ");
+  //int v = digitalRead(2);
+  //Serial.println(v);
+  Serial.println(count);
+  delay(500);
+}
+
+void countButtonPresses() {
+  count++;  // Increase count each time button is pressed
 }
